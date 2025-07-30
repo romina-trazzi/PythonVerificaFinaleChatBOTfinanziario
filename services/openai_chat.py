bdd8d12f8af3f0741793cc0ad27c9019c37a2c82
Romina Trazzi <romina.trazzi@edu-its.it>
2025-07-30 11:31:17 +0200

    Change model

--- services/openai_chat.py
+++ services/openai_chat.py
@@ -16,7 +16,7 @@ HEADERS = {
 
 def ask_llm(message):
     url = "https://openrouter.ai/api/v1/chat/completions"
-    model = "mistralai/mistral-small-3.2-24b-instruct:free"
+    model = "cognitivecomputations/dolphin-mistral-24b-venice-edition:free"
 
     payload = {
         "model": model,
